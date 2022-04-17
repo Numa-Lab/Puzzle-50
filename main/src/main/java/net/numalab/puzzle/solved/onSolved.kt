15d08afebcffa3b6600d7f5abb82902f5ea9c706
bun133 <bun133.work@gmail.com>
2022-04-17 14:05:24 +0900

    Add Title

--- main/src/main/java/net/numalab/puzzle/solved/onSolved.kt
+++ main/src/main/java/net/numalab/puzzle/solved/onSolved.kt
@@ -7,11 +7,13 @@ import com.github.bun133.bukkitfly.component.text
 import com.github.bun133.bukkitfly.entity.firework.spawnFireWork
 import com.github.bun133.bukkitfly.location.circle2D
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.title.Title
 import net.numalab.puzzle.puzzle.Puzzle
 import org.bukkit.*
 import org.bukkit.entity.Player
 import org.bukkit.inventory.meta.FireworkMeta
 import org.bukkit.plugin.Plugin
+import java.time.Duration
 import kotlin.math.PI
 
 private val clearAdv: () -> ToastData = {
@@ -22,22 +24,23 @@ private val clearAdv: () -> ToastData = {
     )
 }
 
-private val clear100Adv: () -> ToastData = {ToastData(
-    Material.DIAMOND,
-    "そこそこのパズルクリア！",
-    ToastData.FrameType.Challenge
-)}
+private val clear100Adv: () -> ToastData = {
+    ToastData(
+        Material.DIAMOND,
+        "そこそこのパズルクリア！",
+        ToastData.FrameType.Challenge
+    )
+}
 
-private val clear1000Adv: () -> ToastData ={ ToastData(
-    Material.EMERALD,
-    "大きなパズルクリア！",
-    ToastData.FrameType.Challenge
-)}
+private val clear1000Adv: () -> ToastData = {
+    ToastData(
+        Material.EMERALD,
+        "大きなパズルクリア！",
+        ToastData.FrameType.Challenge
+    )
+}
 
 fun onSolved(puzzle: Puzzle, location: Location, player: Player, plugin: Plugin) {
-    Bukkit.broadcast(text("パズルが完成しました！", NamedTextColor.GREEN))
-    Bukkit.broadcast(player.displayName() + text("が最後の一ピースをはめました！", NamedTextColor.GREEN))
-
     val f: (FireworkMeta) -> Unit = { m: FireworkMeta ->
         m.power = 1
         m.addEffects(
@@ -52,11 +55,17 @@ fun onSolved(puzzle: Puzzle, location: Location, player: Player, plugin: Plugin)
     val is100 = puzzle.width * puzzle.height >= 100
     val is1000 = puzzle.width * puzzle.height >= 1000
 
-    println("is100: $is100, is1000: $is1000")
-
     Bukkit.getOnlinePlayers().forEach {
         it.toast(clearAdv(), plugin)
         if (is100) it.toast(clear100Adv(), plugin)
         if (is1000) it.toast(clear1000Adv(), plugin)
+
+        it.showTitle(
+            Title.title(
+                text("パズルが完成しました！", NamedTextColor.GREEN),
+                player.displayName() + text("が最後の一ピースをはめました！", NamedTextColor.GREEN),
+                Title.Times.of(Duration.ofSeconds(0), Duration.ofSeconds(5), Duration.ofSeconds(0))
+            )
+        )
     }
 }
